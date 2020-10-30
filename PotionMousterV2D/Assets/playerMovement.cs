19d806f9f1e6497326ee3a08dd8040f5ee9d5f7a
KanWen-G <53582578+KanWen-G@users.noreply.github.com>
2020-10-29 21:13:03 -0700

    Add basic character movement and camera control

---
+++ PotionMousterV2D/Assets/playerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    
+    public float Speed = 1f;
+    public Rigidbody2D RB2D;
+    public Animator animator;
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region PlayerInput
+            movement.x = -Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+        #endregion
+
+        #region animation
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed",movement.sqrMagnitude);
+        #endregion
+
+    }
+    
+    void FixedUpdate(){
+        #region PlayerMovement
+            RB2D.MovePosition(RB2D.position + movement * Speed * Time.fixedDeltaTime);
+            Debug.Log(movement);
+        #endregion
+
+    }
+
+}
